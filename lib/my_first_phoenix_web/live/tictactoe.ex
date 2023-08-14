b6b050155114880f27eefe3f4ac9a9a724e7948e
Joshua Wood <josh.nz@gmail.com>
2023-08-14 20:11:57 +1200

    Improve undo, collapse entire game state to single struct.

--- lib/my_first_phoenix_web/live/tictactoe.ex
+++ lib/my_first_phoenix_web/live/tictactoe.ex
@@ -2,25 +2,32 @@ defmodule MyFirstPhoenixWeb.TicTacToe do
   use MyFirstPhoenixWeb, :live_view
 
   defmodule History do
-    @enforce_keys [:turn, :board, :log, :time_stamp]
-    defstruct [:turn, :board, :log, :time_stamp]
+    @enforce_keys [:turn, :board, :next_player, :status, :log, :time_stamp]
+    defstruct [:turn, :board, :next_player, :status, :log, :time_stamp]
   end
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, new_game())}
+    game = new_game()
+    {:ok, assign(socket, %{
+      current_state: hd(game),
+      game_history: game
+    })}
   end
 
   @impl true
   def handle_event("reset", _, socket) do
-    {:noreply, assign(socket, new_game())}
+    game = new_game()
+    {:noreply, assign(socket, %{
+      current_state: hd(game),
+      game_history: game
+    })}
   end
 
   def handle_event("square-clicked", %{"grid-id" => grid_id_str}, socket) do
     #IO.puts("Handle square clicked")
     grid_id = String.to_integer(grid_id_str)
-    %History{board: board} = hd(socket.assigns.game_state)
-    player = socket.assigns.game_over.next_player
+    %History{board: board, next_player: player} = socket.assigns.current_state
 
     socket = maybe_update_board(socket, grid_id, board, player, board[grid_id])
     #IO.inspect(socket.assigns)
@@ -34,18 +41,18 @@ defmodule MyFirstPhoenixWeb.TicTacToe do
 
   def handle_event("undo", %{"turn" => turn_str}, socket) do
     turn = String.to_integer(turn_str)
-    history = socket.assigns.game_state
+    history = socket.assigns.game_history
     current_turn = length(history)
-    undo_history =
+    new_history =
       case turn do
         x when x >= current_turn or x < 0 -> history  # guard against invalid turn
         _ -> Enum.slice(history, (current_turn - turn - 1)..9)
       end
 
     {:noreply, assign(socket, %{
-      game_state: undo_history,
-      board: hd(undo_history).board})
-    }
+      current_state: hd(new_history),
+      game_history: new_history
+    })}
   end
 
   defp new_game() do
@@ -55,10 +62,14 @@ defmodule MyFirstPhoenixWeb.TicTacToe do
       7 => "", 8 => "", 9 => ""
     }
 
-    %{board: board,
-      game_state: [%History{turn: 0, board: board, log: "Initialised new game.", time_stamp: nz_now()}],
-      game_over: %{status: :undecided, next_player: "X", turn: 0}
-    }
+    [%History{
+      turn: 0,
+      board: board,
+      next_player: "X",
+      status: :undecided,
+      log: "Initialised new game.",
+      time_stamp: nz_now()
+    }]
   end
 
   defp nz_now() do
@@ -68,22 +79,23 @@ defmodule MyFirstPhoenixWeb.TicTacToe do
   defp maybe_update_board(socket, grid_id, board, player, "") do
     new_board = Map.put(board, grid_id, player)
 
+    {status, next_player} = game_over?(new_board, player)
+
     new_game_state = %History{
-      turn: length(socket.assigns.game_state),
+      turn: length(socket.assigns.game_history),
       board: new_board,
+      next_player: next_player,
+      status: status,
       log: "Player #{player} placed in square #{grid_id}",
       time_stamp: nz_now()
     }
 
     #IO.inspect(new_game_state)
 
-    game_over = game_over?(new_board, player)
-
-    assign(socket,
-      board: new_board,
-      game_state: [new_game_state] ++ socket.assigns.game_state,
-      game_over: game_over
-    )
+    assign(socket, %{
+      current_state: new_game_state,
+      game_history: [new_game_state] ++ socket.assigns.game_history
+    })
   end
 
   defp maybe_update_board(socket, _grid_id, _board, _player, _board_value) do
@@ -112,9 +124,9 @@ defmodule MyFirstPhoenixWeb.TicTacToe do
     board_is_full = not Enum.any?(board, fn {_, v} -> v == "" end)
 
     cond do
-      player_has_won -> %{status: :winner, next_player: player}
-      board_is_full -> %{status: :stalemate, next_player: nil}
-      true -> %{status: :undecided, next_player: next_player(player)}
+      player_has_won -> {:winner, player}
+      board_is_full -> {:stalemate, nil}
+      true -> {:undecided, next_player(player)}
     end
   end
 
@@ -125,13 +137,13 @@ defmodule MyFirstPhoenixWeb.TicTacToe do
     ~H"""
     <h1 class="font-bold">Tic Tac Toe</h1>
 
-    <.game_status game_status={@game_over.status} player={@game_over.next_player}  />
+    <.game_status game_status={@current_state.status} player={@current_state.next_player}  />
 
-    <.board_row indexes={[1,2,3]} board={@board} game_status={@game_over.status} />
-    <.board_row indexes={[4,5,6]} board={@board} game_status={@game_over.status} />
-    <.board_row indexes={[7,8,9]} board={@board} game_status={@game_over.status} />
+    <.board_row indexes={[1,2,3]} board={@current_state.board} game_status={@current_state.status} />
+    <.board_row indexes={[4,5,6]} board={@current_state.board} game_status={@current_state.status} />
+    <.board_row indexes={[7,8,9]} board={@current_state.board} game_status={@current_state.status} />
 
-    <.game_history history={@game_state} />
+    <.game_history history={@game_history} />
     """
   end
 
@@ -179,7 +191,7 @@ defmodule MyFirstPhoenixWeb.TicTacToe do
       class="border border-black text-2xl font-bold text-center h-8 w-8 -mt-0.5 -mr-0.5 p-0"
       phx-click="square-clicked"
       phx-value-grid-id={@id}
-      disabled={@game_status != :undecided}
+      disabled={@game_status != :undecided and @state != ""}
       >
       <%= @state %>
     </button>
