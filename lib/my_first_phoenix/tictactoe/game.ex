81f3e3e69bd2a6220177492c78a00a796c88aa95
Joshua Wood <josh.nz@gmail.com>
2023-08-23 18:59:47 +1200

    Add length field validation to game creation.

--- lib/my_first_phoenix/tictactoe/game.ex
+++ lib/my_first_phoenix/tictactoe/game.ex
@@ -2,11 +2,14 @@ defmodule MyFirstPhoenix.Tictactoe.Game do
   import Ecto.Changeset
 
   def changeset(game, attrs \\ %{}) do
-    types = %{title: :string, description: :string}
+    types = %{title: :string, description: :string, player_x: :string, player_o: :string}
     {game, types} |> cast(attrs, Map.keys(types))
   end
 
   def validate(changeset) do
-    validate_required(changeset, [:title])
+    changeset
+      |> validate_required([:title])
+      |> validate_length(:title, max: 50)
+      |> validate_length(:description, max: 200)
   end
 end
