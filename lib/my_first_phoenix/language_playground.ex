1b46a9e6f983cda2ad999edd5e12b6ab6d413f57
Joshua Wood <josh.nz@gmail.com>
2023-08-24 17:26:43 +1200

    Add function pattern match test to language playground.

--- lib/my_first_phoenix/language_playground.ex
+++ lib/my_first_phoenix/language_playground.ex
@@ -1,6 +1,21 @@
 defmodule MyFirstPhoenix.LanguagePlayground do
+  # foo(single_arg) will match this 2 arity fun,
+  # with b = single_arg, a = default.
   def foo(a \\ "default a", b) do
     IO.puts(a)
     IO.puts(b)
   end
+
+  # Fun pattern matching is dependent on fun
+  # ordering. As ordered below, bar(:name) will
+  # print "bar(any)".
+  def bar(any) do
+    IO.puts("bar(any)")
+  end
+
+  def bar(:name) do
+    IO.puts("bar(:name)")
+  end
+
+
 end
